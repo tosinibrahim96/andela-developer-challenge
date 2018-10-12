483828fd888a635f28807fef745af64bc3b78050
ibrahim <tosinibrahim96@gmail.com>
2018-10-12 04:55:59 +0100

    fix all console errors

--- js/main.js
+++ js/main.js
@@ -3,14 +3,16 @@ const sidebar = document.getElementById("sidebar");
 let input, filter, table, tr, td, i;
 input = document.getElementById("myInput");
 
-$(".admin-box").hide();
-$("#show").click(function() {
-	$(".attendant-box").hide();
-	$(".admin-box").fadeIn(200);
-});
-$("#hide").click(function() {
-	$(".attendant-box").fadeIn(200);
+$document.ready(() => {
 	$(".admin-box").hide();
+	$("#show").click(function() {
+		$(".attendant-box").hide();
+		$(".admin-box").fadeIn(200);
+	});
+	$("#hide").click(function() {
+		$(".attendant-box").fadeIn(200);
+		$(".admin-box").hide();
+	});
 });
 
 toggleSidebar = () => {
