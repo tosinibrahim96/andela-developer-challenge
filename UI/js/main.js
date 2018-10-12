de4c46bb6b1521adac5074119994316255822b64
ibrahim <tosinibrahim96@gmail.com>
2018-10-12 04:19:27 +0100

    user interface code submitted

---
+++ UI/js/main.js
@@ -0,0 +1,41 @@
+const togggleButton = document.querySelector(".toggle-btn");
+const sidebar = document.getElementById("sidebar");
+let input, filter, table, tr, td, i;
+input = document.getElementById("myInput");
+
+$(".admin-box").hide();
+$("#show").click(function() {
+	$(".attendant-box").hide();
+	$(".admin-box").fadeIn(200);
+});
+$("#hide").click(function() {
+	$(".attendant-box").fadeIn(200);
+	$(".admin-box").hide();
+});
+
+toggleSidebar = () => {
+	sidebar.classList.toggle("active");
+	sidebar.classList.toggle("show");
+};
+
+searchProduct = () => {
+	// Declare variables
+	filter = input.value.toUpperCase();
+	table = document.querySelector(".searchTable");
+	tr = table.getElementsByTagName("tr");
+
+	// Loop through all table rows, and hide those who don't match the search query
+	for (i = 0; i < tr.length; i++) {
+		td = tr[i].getElementsByTagName("td")[0];
+		if (td) {
+			if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+				tr[i].style.display = "";
+			} else {
+				tr[i].style.display = "none";
+			}
+		}
+	}
+};
+
+togggleButton.addEventListener("click", toggleSidebar);
+input.addEventListener("keyup", searchProduct);
